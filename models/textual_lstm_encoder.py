ae7a3050479e5bfeba7eaf5076b5c39b695669aa
mrsalehi <salehi.mohammadreza1997@gmail.com>
2019-08-20 16:44:23 +0430

    initial commit

---
+++ models/textual_lstm_encoder.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+
+
+
+class textualLSTMEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, input: torch.Tensor):
+        pass
+
---
+++ models/visual_lstm_encoder.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class visualLSTMEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, input: torch.Tensor):
+        pass
